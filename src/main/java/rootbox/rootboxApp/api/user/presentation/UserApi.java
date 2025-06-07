fc096cc2367dc39dbf7fc059de451716f47ff989
CYY1007 <younguk86@gmail.com>
2025-06-07 19:00:54 +0900

    health check 수정

--- src/main/java/rootbox/rootboxApp/api/user/presentation/UserApi.java
+++ src/main/java/rootbox/rootboxApp/api/user/presentation/UserApi.java
@@ -4,6 +4,7 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,6 +17,6 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping(value = "/api/v1/user")
 public class UserApi {
 
-    @PostMapping(value = "/health")
+    @GetMapping(value = "/health")
     public String health() {return "I'm healthy";}
 }
